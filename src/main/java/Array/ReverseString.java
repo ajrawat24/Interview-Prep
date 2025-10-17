d88bf77ad0866795262d060a6b4f8673fc9fe4a3
Ajay Rawat <ajrawat24@gmail.com>
2025-10-17 12:22:14 +0530

    my first commit with basic program

---
+++ src/main/java/Array/ReverseString.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String s="Madam";
+        String lowerCaseString=s.toLowerCase();
+        String reverse="";
+        for(int i=lowerCaseString.length()-1;i>=0;i--){
+            reverse=reverse+lowerCaseString.charAt(i);
+        }
+        System.out.println(reverse);
+
+        if(reverse.equals(lowerCaseString)){
+            System.out.println("Yes String is palindrome");
+        }
+            else{
+                System.out.println("No String is palindrome");
+            }
+        }
+    }
+
+
---
+++ src/main/java/Array/ReverseStringWithStringBuilder.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class ReverseStringWithStringBuilder {
+    public static void main(String[] args) {
+        String s="MadamD";
+        String lowerCaseString=s.toLowerCase();
+        StringBuilder sb=new StringBuilder(lowerCaseString);
+       String  reversed =sb.reverse().toString();
+        //String reversed = new StringBuilder(lowerCaseString).reverse().toString();
+        System.out.println(reversed);
+        if(reversed.equals(lowerCaseString)){
+            System.out.println("String is palindrom");
+        }
+        else {
+            System.out.println("String is not palindrom");
+        }
+
+    }
+
+
+
+
+}
